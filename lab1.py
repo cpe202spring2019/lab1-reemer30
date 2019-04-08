259a04792b484f1391dcd255ef50654c68309651
reemer30 <aidan.lenhart@gmail.com>
2019-04-08 11:04:14 -0700

    max_list_iter

--- lab1.py
+++ lab1.py
@@ -1,15 +1,25 @@
-
 def max_list_iter(int_list):  # must use iteration not recursion
-   """finds the max of a list of numbers and returns the value (not the index)
-   If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+    """finds the max of a list of numbers and returns the value (not the index)
+    If int_list is empty, returns None. If list is None, raises ValueError"""
 
-def reverse_rec(int_list):   # must use recursion
-   """recursively reverses a list of numbers and returns the reversed list
-   If list is None, raises ValueError"""
-   pass
+    if int_list is None:
+        raise ValueError
+    if int_list == []:
+        return None
+    int_max = int_list[0]
+    for i in range(len(int_list)):
+        if int_list[i] > int_max:
+            int_max = int_list[i]
+    return int_max
+
+
+def reverse_rec(int_list):  # must use recursion
+    """recursively reverses a list of numbers and returns the reversed list
+    If list is None, raises ValueError"""
+    pass
+
 
 def bin_search(target, low, high, int_list):  # must use recursion
-   """searches for target in int_list[low..high] and returns index if found
-   If target is not found returns None. If list is None, raises ValueError """
-   pass
+    """searches for target in int_list[low..high] and returns index if found
+    If target is not found returns None. If list is None, raises ValueError """
+    pass
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -1,25 +1,50 @@
-aimport unittest
+import unittest
 from lab1 import *
 
- # A few test cases.  Add more!!!
+
+# A few test cases.  Add more!!!
 class TestLab1(unittest.TestCase):
 
-    def test_max_list_iter(self):
-        """add description here"""
+    def test_max_None(self):
+        """Should raise ValueError when passed None"""
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
 
-    def test_reverse_rec(self):
-        self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+    def test_max_empty(self):
+        """Returns None when passed an empty list"""
+        tlist = []
+        self.assertEqual(max_list_iter(tlist), None)
 
-    def test_bin_search(self):
-        list_val =[0,1,2,3,4,7,8,9,10]
-        low = 0
-        high = len(list_val)-1
-        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
+    def test_max_std(self):
+        """Tests a variety of lists containing zeroes, negatives, and positive values, in varying lengths"""
+        tlist =  [1, 2, 3, 4, 5, 6, 7, 12]
+        tlist2 = [12, 2, 3, 12, 5, 6, 7, 12]
+        tlist3 = [12, 4, 5, 6, 7]
+        tlist4 = [-1, -4, -5, -6, -7, -12]
+        tlist5 = [-1, 2, -3, -4, 5, -6, 7, -12]
+        tlist6 = [2, 4, -3, 0, -9, 0, 2, 4]
+        tlist7 = [8]
+        tlist8 = [-2, 0, -9, -2]
+
+        self.assertEqual(max_list_iter(tlist), 12)
+        self.assertEqual(max_list_iter(tlist2), 12)
+        self.assertEqual(max_list_iter(tlist3), 12)
+        self.assertEqual(max_list_iter(tlist4), -1)
+        self.assertEqual(max_list_iter(tlist5), 7)
+        self.assertEqual(max_list_iter(tlist6), 4)
+        self.assertEqual(max_list_iter(tlist7), 8)
+        self.assertEqual(max_list_iter(tlist8), 0)
+
+
+#    def test_reverse_rec(self):
+#        self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+#
+#    def test_bin_search(self):
+#        list_val =[0,1,2,3,4,7,8,9,10]
+#        low = 0
+#        high = len(list_val)-1
+#        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
 
 if __name__ == "__main__":
-        unittest.main()
-
-    
+    unittest.main()
